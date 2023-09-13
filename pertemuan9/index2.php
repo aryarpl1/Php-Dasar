ca5776917f4a6c9bb557a92bee1f4286482bdf74
aryarpl1 <144863381+aryarpl1@users.noreply.github.com>
2023-09-13 19:41:41 +0700

    Add files via upload

---
+++ pertemuan9/index2.php
@@ -0,0 +1,62 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "root", "phpdasar");
+
+// ambil data dari tabel mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+// ambil data (fetch) mahasiswa dari object result
+// mysqli_fetch_row() // mengembalikan dari object result
+// mysqli_fetch_assoc() // mengembalikan array associative
+// mysqli_fetch_array() // mengembalikan keduanya
+// mysqli_fetch_object()
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+    <h1>Daftar Mahasiswa</h1>
+
+    <table border="1" cellpedding="10" cellspacing="0">
+
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NRP</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php while( $row = mysqli_fetch_assoc($result) ) : ?>
+    <tr>
+        <td><?= $i; ?></td>
+        <td>
+            <a href="">ubah</a> |
+            <a href="">hapus</a>
+        </td>
+        <td><img src="image/<?= $row["gambar"]; ?>"></td>
+        <td><?= $row["nrp"]; ?></td>
+        <td><?= $row["nama"]; ?></td>
+        <td><?= $row["email"]; ?></td>
+        <td><?= $row["jurusan"]; ?></td>
+    </tr>
+    <?php $i++; ?>
+    <?php endwhile; ?>
+
+    </table>
+
+</body>
+</html>
